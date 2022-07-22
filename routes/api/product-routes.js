3e207f07c6170e5f42268318d6214980cc3d318d
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-07-21 23:54:03 -0600

    create get route for finiding product by id. refactor get route for all products, fixed the include model bug

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     // find all products
     const productData = await Product.findAll({
       // be sure to include its associated Category and Tag data
-      include: [{ model: Category, Tag }],
+      include: [{ model: Category}, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -18,9 +18,22 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+     // find a single product by its `id`
+    const productData = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category}, { model: Tag }],
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'No product with that id!'})
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
