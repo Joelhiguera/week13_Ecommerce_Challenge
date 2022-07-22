73661ea228a6c12fc5276b64d65ddfcc4ecd42de
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-07-21 22:37:19 -0600

    create get route for finding a category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,10 +20,22 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const categoryData = await 
+    // find one category by its `id` value
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product}],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category with that id!' })
+      return;
+    }
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
   }
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  
+  
 });
 
 router.post('/', (req, res) => {
