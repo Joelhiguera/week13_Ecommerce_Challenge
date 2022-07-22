da518bd24b74c744f96e5f2749f3dbde8af70700
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-07-22 00:03:45 -0600

    create get route to find all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,6 +4,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    
+    const tagData = await Tag.findAll({
+      
+      include: [{ model: Product }, { model: ProductTag }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // find all tags
   // be sure to include its associated Product data
 });
